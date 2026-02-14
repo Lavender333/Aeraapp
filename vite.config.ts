9cf14e8e67f7104d6874093c50970aad06b034df
Lavender333 <action@github.com>
2026-02-13 19:16:51 -0500

    Add Vite legacy bundles to fix blank screens on older browsers

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
+import legacy from '@vitejs/plugin-legacy';
 
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
@@ -11,7 +12,13 @@ export default defineConfig(({ mode }) => {
         port: 3000,
         host: '0.0.0.0',
       },
-      plugins: [react()],
+      plugins: [
+        react(),
+        legacy({
+          targets: ['defaults', 'not IE 11', 'iOS >= 12'],
+          modernPolyfills: true,
+        }),
+      ],
       build: {
         outDir: 'dist',
         sourcemap: false,
